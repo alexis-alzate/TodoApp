6ec7ee0d6aa9586e65c675554c601dff259ab45a
alexis-alzate <alexismejor10@hotmail.es>
2025-07-29 18:08:09 -0500

    Iniciando proyect, importando librerias

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = requeire('express');
+const mongoose = requeire('mongoose');
+const cors = requiere('cors');
+const dotenv = requeire('dotenv');
+const taskRoutes = requeire(',/routes/tasks');
+const errorHandler = requeire(',/middleware/errorHanler');
+
