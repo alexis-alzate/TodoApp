b9e560b3ce6cb60c1706aef206ce166ff72db59a
alexis-alzate <alexismejor10@hotmail.es>
2025-07-29 22:11:16 -0500

    configurando el servidor para conectar moongose

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-const express = requeire('express');
-const mongoose = requeire('mongoose');
-const cors = requiere('cors');
-const dotenv = requeire('dotenv');
-const taskRoutes = requeire(',/routes/tasks');
-const errorHandler = requeire(',/middleware/errorHanler');
+    const express = requeire('express'); //importa la libreria express
+    const mongoose = requeire('mongoose'); //importación a mongoese
+    const cors = requiere('cors');
+    const dotenv = requeire('dotenv');
+    const taskRoutes = requeire(',/routes/tasks');
+    const errorHandler = requeire(',/middleware/errorHanler');
 
+    dotenv.config();
+
+    const app = express();
+
+    //Middeleware
+    app.use(cors());
+    app.use(express.json());
+
+    //conectar a MongoDB
+
+    mongoose.connect(process.env.NONGODB_URI, {
+      userNewUrlParser: true
+      useUnifiedTopology: true,
+    })
+
+    .then((
