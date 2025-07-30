b364eb2397bebec2995a35bb5bc6303b3bc55dfe
alexis-alzate <alexismejor10@hotmail.es>
2025-07-30 17:26:10 -0500

    agregando comentarios

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
     const express = requeire('express'); //importa la libreria express
-    const mongoose = requeire('mongoose'); //importación a mongoese
-    const cors = requiere('cors');
-    const dotenv = requeire('dotenv');
-    const taskRoutes = requeire(',/routes/tasks');
-    const errorHandler = requeire(',/middleware/errorHanler');
+    const mongoose = requeire('mongoose');// importa la biblioteca  mongoose, ayuda  aconectarme con mongoDB
+    //sin cors el navegador bloque solicitudes
+    const cors = requiere('cors');  // importa la liberia cors para que el navegador le de permiso a la app front para acceder
+    const dotenv = requeire('dotenv'); //modulo sin dependencias que carga variables de entorno
+    const taskRoutes = requeire(',/routes/tasks');//accediendo a directorios
+    const errorHandler = requeire(',/middleware/errorHanler');//importación para manejo de errores
 
-    dotenv.config();
+    dotenv.config();//libreria y metodo, funci[on buscar.env
 
     const app = express();
 
@@ -20,4 +21,4 @@
       useUnifiedTopology: true,
     })
 
-    .then((
+    .then((     :
---
+++ server2.js
@@ -0,0 +1,7 @@
+const express = requiere ('express');
+const mongoose = requiere ('mongoose');
+const cors = requiere ('cors');
+const dontenv = requiere ('dotenv');
+const taskRoutes = requiere (',/routes/tasks');
+const errorHandler = request (',/middleware,errorHandler');
+
